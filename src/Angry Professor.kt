4fbea497c648902c416e0563fee448024c5fd541
dogukankeskinoglu <dogukan.keskinoglu.ege@gmail.com>
2021-05-24 17:32:34 +0300

    Add New Problem Solution

--- src/Angry Professor.kt
+++ src/Angry Professor.kt
@@ -3,7 +3,6 @@ fun main(){
     val input="-1 -3 4 2"
     val inputFormat=setInput(input)
     print(angryProfessor(k,inputFormat))
-
 }
 fun setInput(input:String):MutableList<Int>{
     return input.trimEnd().split(" ").map{ it.toInt() }.toMutableList()
